077576be9b272127212f32dbe78fbe709a90b7ff
MatkoKardum <matejkardum1@gmail.com>
2023-01-27 16:39:35 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--0');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+let score, activePlayer, scores, playing;
+
+const init = function () {
+  score = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  document.querySelector(`.player--0`).classList.remove('player--winner');
+  document.querySelector(`.player--1`).classList.remove('player--winner');
+};
+
+init();
+
+const switchPlayer = function () {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const diceRoll = Math.floor(Math.random() * 6) + 1;
+    console.log(diceRoll);
+    diceEl.classList.remove('hidden');
+    diceEl.src = `./img/dice-${diceRoll}.png`;
+
+    if (diceRoll !== 1) {
+      score += diceRoll;
+      document.querySelector(`#current--${activePlayer}`).textContent = score;
+    } else {
+      switchPlayer();
+      score = 0;
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += score;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 20) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      playing = false;
+    }
+
+    switchPlayer();
+    score = 0;
+  }
+});
+
+btnNew.addEventListener('click', init);
